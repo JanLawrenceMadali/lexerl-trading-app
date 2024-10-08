32b517ea62112d2da03fd03262eb0d095223fafc
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-10-08 09:18:15 +0800

    Create ErrorToast.vue

---
+++ resources/js/Components/ErrorToast.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ul class="mt-1 text-sm">
+            <li v-for="(message, index) in messages" :key="index">
+                {{ message }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    messages: {
+        type: Array,
+        required: true
+    }
+})
+</script>
