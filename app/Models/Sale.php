f319b0603ca2d233dd52926f8bdbe5b9041b3037
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-10-13 23:26:05 +0800

    new models added

---
+++ app/Models/Sale.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sale extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'notes',
+        'amount',
+        'is_paid',
+        'quantity',
+        'sales_date',
+        'landed_cost',
+        'customer_id',
+        'due_date_id',
+        'category_id',
+        'subcategory_id',
+        'transaction_id',
+        'unit_measure_id',
+        'transaction_number',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+
+    public function unit_measure()
+    {
+        return $this->belongsTo(UnitMeasure::class);
+    }
+
+    public function due_date()
+    {
+        return $this->belongsTo(DueDate::class);
+    }
+}
