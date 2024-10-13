08d0fd51d26138a6e17f9468f75984133b76de54
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-10-13 23:26:50 +0800

    Update PurchaseFactory.php

--- database/factories/PurchaseFactory.php
+++ database/factories/PurchaseFactory.php
@@ -18,13 +18,15 @@ class PurchaseFactory extends Factory
     {
         return [
             'quantity' => fake()->randomNumber(2, true),
-            'cost' => fake()->randomFloat(2, 1, 1000),
+            'landed_cost' => fake()->randomFloat(2, 1, 1000),
+            'amount' => fake()->randomFloat(2, 1, 1000),
             'notes' => fake()->sentence(),
             'category_id' => rand(1, 4),
             'supplier_id' => rand(1, 30),
             'subcategory_id' => rand(1, 2),
             'transaction_id' => rand(1, 3),
             'unit_measure_id' => rand(1, 7),
+            'purchase_date' => fake()->dateTimeBetween('-1 year', 'now'),
             'transaction_number' => fake()->unique()->randomNumber(5, true)
         ];
     }
