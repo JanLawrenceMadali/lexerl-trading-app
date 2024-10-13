f2579c4073d1a6edcc80207b6dfe8b982b27c3b2
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-10-13 23:26:32 +0800

    Update PurchaseInController.php

--- app/Http/Controllers/PurchaseInController.php
+++ app/Http/Controllers/PurchaseInController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PurchaseInRequest;
 use App\Models\Category;
 use App\Models\Purchase;
 use App\Models\Subcategory;
@@ -24,79 +25,29 @@ class PurchaseInController extends Controller
         ]);
     }
 
-    public function create()
-    {
-        return inertia('Transactions/PurchaseIn/Create', [
-            'units' => UnitMeasure::all(),
-            'categories' => Category::all(),
-            'transactions' => Transaction::all(),
-            'subcategories' => Subcategory::all(),
-            'suppliers' => Supplier::latest()->get(),
-        ]);
-    }
-
-    public function store(Request $request)
+    public function store(Request $request, PurchaseInRequest $purchaseInRequest)
     {
         sleep(1);
-        $request->validate([
-            'cost' => 'required',
-            'quantity' => 'required',
-            'category_id' => 'required',
-            'supplier_id' => 'required',
-            'subcategory_id' => 'required',
-            'transaction_id' => 'required',
-            'unit_measure_id' => 'required',
-            'transaction_number' => 'required | unique:purchases',
-        ], [
-            'cost.required' => 'The cost field is required.',
-            'quantity.required' => 'The quantity field is required.',
-            'category_id.required' => 'The category field is required.',
-            'supplier_id.required' => 'The supplier field is required.',
-            'subcategory_id.required' => 'The subcategory field is required.',
-            'transaction_id.required' => 'The transaction type field is required.',
-            'unit_measure_id.required' => 'The unit measure field is required.',
-            'transaction_number.required' => 'The transaction number field is required.',
-        ]);
+        $validated = $purchaseInRequest->validated();
 
         Purchase::create([
-            'cost' => $request->cost,
+            ...$validated,
             'notes' => $request->notes,
-            'quantity' => $request->quantity,
-            'category_id' => $request->category_id,
-            'supplier_id' => $request->supplier_id,
-            'subcategory_id' => $request->subcategory_id,
-            'transaction_id' => $request->transaction_id,
-            'unit_measure_id' => $request->unit_measure_id,
-            'transaction_number' => $request->transaction_number,
+            'amount' => $request->amount,
         ]);
 
         return redirect()->route('purchase-in')->with('message', 'Purchase In created successfully');
     }
 
-    public function update(Request $request, Purchase $purchase)
+    public function update(Request $request, PurchaseInRequest $purchaseInRequest, Purchase $purchase)
     {
         sleep(1);
-        $request->validate([
-            'cost' => 'required',
-            'quantity' => 'required',
-            'category_id' => 'required',
-            'supplier_id' => 'required',
-            'subcategory_id' => 'required',
-            'transaction_id' => 'required',
-            'unit_measure_id' => 'required',
-            'transaction_number' => 'required',
-        ]);
+        $validated = $purchaseInRequest->validated();
 
         $purchase->update([
-            'cost' => $request->cost,
+            ...$validated,
             'notes' => $request->notes,
-            'quantity' => $request->quantity,
-            'category_id' => $request->category_id,
-            'supplier_id' => $request->supplier_id,
-            'subcategory_id' => $request->subcategory_id,
-            'transaction_id' => $request->transaction_id,
-            'unit_measure_id' => $request->unit_measure_id,
-            'transaction_number' => $request->transaction_number,
+            'amount' => $request->amount
         ]);
 
         return redirect()->route('purchase-in')->with('message', 'Purchase In updated successfully');
