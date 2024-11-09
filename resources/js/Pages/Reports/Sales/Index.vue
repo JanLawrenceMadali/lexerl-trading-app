db2d129bd45d9acfff7e5085d008fac3e02445f4
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-11-09 22:01:59 +0800

    Create Sale reports

---
+++ resources/js/Pages/Reports/Sales/Index.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from "vue";
+import Datatable from './Datatable.vue';
+import Header from '@/Components/Header.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+const props = defineProps({
+    sales: Object
+})
+
+const items = ref([
+    { label: 'Reports', href: '#' },
+    { label: 'Sale Logs', href: 'sale_logs' },
+])
+</script>
+
+<template>
+    <Head title="Reports" />
+    <AuthenticatedLayout>
+        <Header :items="items" />
+        <div class="px-4 py-4 sm:py-0 md:gap-8 md:px-6">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Sales</CardTitle>
+                    <CardDescription class="flex items-center justify-between">
+                        Manage your purchases and view their information.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Datatable :sales="sales" />
+                </CardContent>
+                <CardFooter>
+                </CardFooter>
+            </Card>
+        </div>
+    </AuthenticatedLayout>
+</template>
