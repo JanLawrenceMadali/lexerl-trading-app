d281a5881fe5d33fa407fae8d9b00a7684e788e8
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-11-09 22:02:03 +0800

    Update Index.vue

--- resources/js/Pages/Settings/Index.vue
+++ resources/js/Pages/Settings/Index.vue
@@ -1,8 +1,4 @@
 <script setup>
-import { Button } from '@/Components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/Components/ui/card';
-import { Checkbox } from '@/Components/ui/checkbox';
-import { Input } from '@/Components/ui/input';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 </script>
 
@@ -10,62 +6,26 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
     <AuthenticatedLayout>
         <main
             class="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-            <div class="grid w-full max-w-6xl gap-2 mx-auto">
+            <div class="grid w-full max-w-full gap-2 mx-auto">
                 <h1 class="text-3xl font-semibold">
                     Settings
                 </h1>
             </div>
             <div
-                class="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+                class="mx-auto grid w-full max-w-full items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
                 <nav class="grid gap-4 text-sm text-muted-foreground">
-                    <a href="#" class="font-semibold text-primary">General</a>
-                    <Link :href="route('settings.users')">Manage users</Link>
-                    <a href="#">Integrations</a>
+                    <Link :href="route('settings.users')"
+                        :class="{ 'font-semibold text-primary': $page.component === 'Settings/Users/Index' }">Manage users
+                    </Link>
+                    <Link :href="route('settings.suppliers')"
+                        :class="{ 'font-semibold text-primary': $page.component === 'Settings/Suppliers/Index' }">Manage suppliers
+                    </Link>
                     <a href="#">Support</a>
                     <a href="#">Organizations</a>
                     <a href="#">Advanced</a>
                 </nav>
                 <div class="grid gap-6">
-                    <Card>
-                        <CardHeader>
-                            <CardTitle>Store Name</CardTitle>
-                            <CardDescription>
-                                Used to identify your store in the marketplace.
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            <form>
-                                <Input placeholder="Store Name" />
-                            </form>
-                        </CardContent>
-                        <CardFooter class="px-6 py-4 border-t">
-                            <Button>Save</Button>
-                        </CardFooter>
-                    </Card>
-                    <Card>
-                        <CardHeader>
-                            <CardTitle>Plugins Directory</CardTitle>
-                            <CardDescription>
-                                The directory within your project, in which your plugins are
-                                located.
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            <form class="flex flex-col gap-4">
-                                <Input placeholder="Project Name" default-value="/content/plugins" />
-                                <div class="flex items-center space-x-2">
-                                    <Checkbox id="include" default-checked />
-                                    <label for="include"
-                                        class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                                        Allow administrators to change the directory.
-                                    </label>
-                                </div>
-                            </form>
-                        </CardContent>
-                        <CardFooter class="px-6 py-4 border-t">
-                            <Button>Save</Button>
-                        </CardFooter>
-                    </Card>
+                    <slot />
                 </div>
             </div>
         </main>
