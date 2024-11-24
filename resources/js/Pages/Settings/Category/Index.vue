a6e8ae99c9ebae26256c790704cbe0494fc3a90e
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-11-25 02:01:53 +0800

    Create category page

---
+++ resources/js/Pages/Settings/Category/Index.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from "vue";
+import Datatable from './Datatable.vue';
+import Settings from '../Index.vue'
+
+const props = defineProps({
+    categories: Object
+})
+
+const items = ref([
+    { label: 'Settings', href: 'settings' },
+    { label: 'Category', href: 'categories' },
+])
+</script>
+
+<template>
+    <Head title="Manage Category" />
+    <Settings>
+        <div class="px-4">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Category</CardTitle>
+                    <CardDescription class="flex items-center justify-between">
+                        Manage categories and view their information.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Datatable :categories="categories" />
+                </CardContent>
+                <CardFooter>
+                </CardFooter>
+            </Card>
+        </div>
+    </Settings>
+</template>
