d08c3b643206358e1b9dcd114695dceee654ee97
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-11-25 02:27:05 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,41 @@
 <?php
 
+use App\Http\Controllers\BackupController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CollectibleController;
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\Setting\UserController;
 use App\Http\Controllers\SubCategoryController;
 use App\Http\Controllers\SupplierController;
 use App\Models\ActivityLog;
+use App\Models\Inventory;
 use App\Models\Sale;
-use App\Models\User;
-use Illuminate\Support\Facades\DB;
-use Inertia\Inertia;
 
 Route::get('/', function () {
     return redirect(route('login'));
 });
 
 Route::get('/dashboard', function () {
-    $users = User::all();
-    $sales = Sale::all();
+    $sale = Sale::all();
+    $total_sale = $sale->sum('total_amount');
+    $total_purchase = Inventory::sum('amount');
+    $total_collectible = $sale->where('status_id', 2)->sum('total_amount');
+    $activity_logs = ActivityLog::with('users')->latest()->get();
 
     return inertia('Dashboard', [
-        'users' => $users->count(),
-        'sales' => $sales->sum('total_amount')
+        'total_sale' => $total_sale,
+        'total_purchase' => $total_purchase,
+        'total_collectible' => $total_collectible,
+        'activity_logs' => $activity_logs
     ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -43,6 +49,7 @@ Route::middleware('auth')->group(function () {
         Route::post('/store', [InventoryController::class, 'store'])->name('purchase-in.store');
         Route::patch('/update/{inventory}', [InventoryController::class, 'update'])->name('purchase-in.update');
         Route::delete('/destroy/{inventory}', [InventoryController::class, 'destroy'])->name('purchase-in.destroy');
+        Route::get('/export', [InventoryController::class, 'export'])->name('purchase-in.export');
     });
 
     // Sales
@@ -51,97 +58,67 @@ Route::middleware('auth')->group(function () {
         Route::post('/store', [SalesController::class, 'store'])->name('sales.store');
         Route::patch('/update/{sale}', [SalesController::class, 'update'])->name('sales.update');
         Route::delete('/destroy/{sale}', [SalesController::class, 'destroy'])->name('sales.destroy');
+        Route::get('/export', [SalesController::class, 'export'])->name('sales.export');
     });
 
     // Collectibles
     Route::prefix('collectibles')->group(function () {
         Route::get('/', [CollectibleController::class, 'index'])->name('collectibles');
         Route::post('/update', [CollectibleController::class, 'update'])->name('collectibles.update');
+        Route::get('/export', [CollectibleController::class, 'export'])->name('collectibles.export');
     });
 
     // Reports
-    Route::get('/activity_logs', function () {
-        return Inertia::render('Reports/ActivityLogs/Index', [
-            'logs' => ActivityLog::with('users')->latest()->get()
-        ]);
-    })->name('activity_logs');
-    // Inventory
-    Route::get('/inventory_logs', function () {
-        $inventories = DB::table('inventories')
-            ->join('units', 'inventories.unit_id', '=', 'units.id')
-            ->join('categories', 'inventories.category_id', '=', 'categories.id')
-            ->join('subcategories', 'inventories.subcategory_id', '=', 'subcategories.id')
-            ->select(
-                'units.abbreviation as unit',
-                'categories.name as category_name',
-                'subcategories.name as subcategory_name',
-                DB::raw('SUM(inventories.quantity) as total_quantity')
-            )
-            ->groupBy('units.abbreviation', 'categories.name', 'subcategories.name')
-            ->having('total_quantity', '>', 0)
-            ->latest('inventories.created_at')
-            ->get()
-            ->map(function ($item) {
-                return [
-                    'category_name' => $item->category_name,
-                    'subcategory_name' => $item->subcategory_name,
-                    'unit' => $item->unit,
-                    'quantity' => $item->total_quantity . ' left'
-                ];
-            })
-            ->filter(function ($item) {
-                return intval($item['quantity']) > 0;
-            });
+    Route::get('/activity_logs', [ReportController::class, 'activity_logs'])->name('activity_logs');
+    Route::get('/activity_logs/export', [ReportController::class, 'activity_logs_export'])->name('activity_logs.export');
+    Route::get('/current_inventory', [ReportController::class, 'current_inventory'])->name('current_inventory');
+    Route::get('/current_inventory/export', [ReportController::class, 'current_inventory_export'])->name('current_inventory.export');
+    Route::get('/sale_logs', [ReportController::class, 'sale_logs'])->name('sale_logs');
 
-        return Inertia::render('Reports/Inventory/Index', [
-            'inventories' => $inventories
-        ]);
-    })->name('inventory_logs');
-
-    // Sales
-    Route::get('/sale_logs', function () {
-        $sales = DB::table('product_sale')
-            ->join('products', 'product_sale.product_id', '=', 'products.id')
-            ->join('categories', 'products.category_id', '=', 'categories.id')
-            ->join('subcategories', 'products.subcategory_id', '=', 'subcategories.id')
-            ->join('units', 'product_sale.unit_id', '=', 'units.id')
-            ->select(
-                'categories.name as category_name',
-                'subcategories.name as subcategory_name',
-                'product_sale.quantity',
-                'units.abbreviation as unit'
-            )
-            ->latest('product_sale.created_at')
-            ->get();
-
-        return Inertia::render('Reports/Sales/Index', [
-            'sales' => $sales
-        ]);
-    })->name('sale_logs');
     // Settings
-    Route::inertia('/settings', 'Settings/Index')->name('settings');
+    Route::prefix('users')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('users');
+        // Route::post('/store', [UserController::class, 'store'])->name('user.store');
+        // Route::patch('/update/{user}', [UserController::class, 'update'])->name('user.update');
+        // Route::delete('/destroy/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+    });
     // Supplier
     Route::prefix('suppliers')->group(function () {
         Route::get('/', [SupplierController::class, 'index'])->name('settings.suppliers');
         Route::post('/store', [SupplierController::class, 'store'])->name('supplier.store');
-        // Route::patch('/update/{supplier}', [SupplierController::class, 'update'])->name('supplier.update');
-        // Route::delete('/destroy/{supplier}', [SupplierController::class, 'destroy'])->name('supplier.destroy');
+        Route::patch('/update/{supplier}', [SupplierController::class, 'update'])->name('supplier.update');
+        Route::delete('/destroy/{supplier}', [SupplierController::class, 'destroy'])->name('supplier.destroy');
     });
     // Customer
-    Route::prefix('customer')->group(function () {
-        Route::get('/', [CustomerController::class, 'index'])->name('settings.customer');
+    Route::prefix('customers')->group(function () {
+        Route::get('/', [CustomerController::class, 'index'])->name('settings.customers');
         Route::post('/store', [CustomerController::class, 'store'])->name('customer.store');
-        // Route::patch('/update/{customer}', [SupplierController::class, 'update'])->name('customer.update');
-        // Route::delete('/destroy/{customer}', [SupplierController::class, 'destroy'])->name('customer.destroy');
+        Route::patch('/update/{customer}', [CustomerController::class, 'update'])->name('customer.update');
+        Route::delete('/destroy/{customer}', [CustomerController::class, 'destroy'])->name('customer.destroy');
+    });
+    // Category
+    Route::prefix('categories')->group(function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('settings.categories');
+        Route::post('/store', [CategoryController::class, 'store'])->name('categories.store');
+        // Route::patch('/update/{categories}', [SubcategoryController::class, 'update'])->name('categories.update');
+        // Route::delete('/destroy/{categories}', [SubcategoryController::class, 'destroy'])->name('categories.destroy');
     });
     // Subcategory
     Route::prefix('subcategories')->group(function () {
         Route::get('/', [SubCategoryController::class, 'index'])->name('settings.subcategories');
-        Route::post('/store', [SubcategoryController::class, 'store'])->name('subcategory.store');
+        Route::post('/store', [SubCategoryController::class, 'store'])->name('subcategory.store');
         // Route::patch('/update/{subcategory}', [SubcategoryController::class, 'update'])->name('subcategory.update');
         // Route::delete('/destroy/{subcategory}', [SubcategoryController::class, 'destroy'])->name('subcategory.destroy');
     });
-    Route::get('/users', [UserController::class, 'index'])->name('settings.users');
+
+    // Backup
+    Route::prefix('backup')->group(function () {
+        Route::get('/', [BackupController::class, 'index'])->name('backup');
+        Route::post('/manual', [BackupController::class, 'manualBackup'])->name('backup.manual');
+        Route::post('/download', [BackupController::class, 'download'])->name('backup.download');
+        Route::delete('/clean-all', [BackupController::class, 'cleanAll'])->name('backup.clean-all');
+        Route::post('/restore', [BackupController::class, 'restore'])->name('backup.restore');
+    });
 });
 
 require __DIR__ . '/auth.php';
