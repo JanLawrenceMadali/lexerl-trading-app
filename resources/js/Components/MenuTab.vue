7faf41116821229fce4c312447227186b726b26d
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-11-25 02:25:04 +0800

    Update MenuTab.vue

--- resources/js/Components/MenuTab.vue
+++ resources/js/Components/MenuTab.vue
@@ -1,24 +1,16 @@
 <script setup>
 defineProps({
-    label: {
-        type: String,
-        required: true,
-    },
-    icon: {
-        type: [Object, Function]
-    },
-    route: {
-        type: String,
-        required: false,
-        default: null
-    }
+    label: { type: String, required: true },
+    icon: { type: [Object, Function] },
+    route: { type: String, required: false, default: null },
+    customClass: { type: String, required: false, default: null }
 })
 
 </script>
 
 <template>
-    <Link :href="route" :class="['grid place-items-center gap-3 px-3 py-2 mx-1 text-slate-100 hover:bg-slate-100 transition-all rounded hover:text-primary',
-        $page.component === label ? 'bg-slate-100 text-slate-800' : 'text-muted-foreground']">
+    <Link :href="route"
+        :class="[customClass, 'grid place-items-center gap-3 px-3 py-2 mx-1 text-slate-100 hover:bg-slate-100 transition-all rounded hover:text-primary']">
     <component :is="icon" class="size-8" />
     <span class="text-sm">{{ label }}</span>
     </Link>
