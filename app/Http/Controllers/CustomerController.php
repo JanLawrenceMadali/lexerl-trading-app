a8ca8a1c19d29e1fb886aa2ce95a7241bd609d0e
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2025-01-29 22:08:18 +0800

    Remove unused DB facade import from CustomerController

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use App\Models\ActivityLog;
-use Illuminate\Support\Facades\DB;
 use App\Http\Requests\CustomerRequest;
 use App\Services\ActivityLoggerService;
 use App\Services\CustomerService;
